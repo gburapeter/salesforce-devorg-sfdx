6b2e28763106f0879febf2ca89812963da31c32f
ATREMA\pbura <gburapeti@gmail.com>
2021-10-20 10:27:24 +0200

    hi

---
+++ force-app/main/default/lwc/textLWCLab/textLWCLab.js
@@ -0,0 +1,10 @@
+/**
+ * Created by pbura on 10/20/2021.
+ */
+
+import { LightningElement } from 'lwc';
+
+export default class TextLwcLab extends LightningElement {
+    shouldBeDisplayed = false;
+
+}
